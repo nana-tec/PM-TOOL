c18252d95029d2c58d2db71bf478beaca191fdd4
venomous-maker <morganokumu8+1@gmail.com>
2025-11-29 20:43:12 +0300

    Add parent-child relationship serialization to ProjectResource

--- app/Http/Resources/Project/ProjectResource.php
+++ app/Http/Resources/Project/ProjectResource.php
@@ -25,6 +25,29 @@ class ProjectResource extends JsonResource
             'all_tasks_count' => $this->all_tasks_count,
             'completed_tasks_count' => $this->completed_tasks_count,
             'overdue_tasks_count' => $this->overdue_tasks_count,
+            'parent_id' => $this->parent_id,
+            'children_count' => $this->whenLoaded('children', fn () => $this->children->count()),
+            'children' => $this->whenLoaded('children', fn () => $this->serializeChildren($this->children)),
         ];
+    }
+
+    /**
+     * @param \Illuminate\Support\Collection<int, \App\Models\Project> $children
+     * @return array<int, array<string, mixed>>
+     */
+    protected function serializeChildren($children): array
+    {
+        return $children->map(function ($child) {
+            return [
+                'id' => $child->id,
+                'name' => $child->name,
+                'parent_id' => $child->parent_id,
+                'children_count' => $child->relationLoaded('children') ? $child->children->count() : 0,
+                'tasks_count' => method_exists($child, 'tasks') ? $child->tasks()->count() : 0,
+                'children' => $child->relationLoaded('children')
+                    ? $this->serializeChildren($child->children)
+                    : [],
+            ];
+        })->toArray();
     }
 }
